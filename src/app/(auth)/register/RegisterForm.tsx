37340fff66d0fdc117cc3dc81584389d92140446
Ivan Panchenko <arigatory@gmail.com>
2024-06-30 21:58:48 +0300

    feat: add register form

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -1,39 +1,45 @@
-"use client";
-import { LoginSchema, loginSchema } from "@/lib/schemas/loginSchema";
+import { RegisterSchema, registerSchema } from "@/lib/schemas/registerSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { GiPadlock } from "react-icons/gi";
 
-export default function LoginForm() {
+export default function RegisterForm() {
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm<LoginSchema>({
-    resolver: zodResolver(loginSchema),
-    mode: 'onTouched'
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    mode: "onTouched",
   });
 
-  const onSubmit = (data: LoginSchema) => {
+  const onSubmit = (data: RegisterSchema) => {
     console.log(data);
   };
-
   return (
     <Card className=" w-2/5 mx-auto">
       <CardHeader className=" flex flex-col items-center justify-center">
         <div className=" flex flex-col gap-2 items-center text-secondary">
           <div className=" flex flex-row items-center gap-3">
             <GiPadlock size={30} />
-            <h1 className=" text-3xl font-semibold">Login</h1>
+            <h1 className=" text-3xl font-semibold">Register</h1>
           </div>
-          <p className=" text-neutral-500">Welcome back to NextMatch</p>
+          <p className=" text-neutral-500">Welcome to NextMatch</p>
         </div>
       </CardHeader>
       <CardBody>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className=" space-y-4">
+            <Input
+              defaultValue=""
+              label="Name"
+              variant="bordered"
+              {...register("name")}
+              isInvalid={!!errors.name}
+              errorMessage={errors.name?.message as string}
+            />
             <Input
               defaultValue=""
               label="Email"
@@ -51,8 +57,13 @@ export default function LoginForm() {
               isInvalid={!!errors.password}
               errorMessage={errors.password?.message as string}
             />
-            <Button isDisabled={!isValid} fullWidth color="secondary" type="submit">
-              Login
+            <Button
+              isDisabled={!isValid}
+              fullWidth
+              color="secondary"
+              type="submit"
+            >
+              Register
             </Button>
           </div>
         </form>
