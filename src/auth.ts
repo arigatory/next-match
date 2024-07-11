c1f43763e3a20ee9ac8e7a5062f9e68b97133862
Ivan Panchenko <arigatory@gmail.com>
2024-07-11 20:21:34 +0300

    add all auth logic

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,48 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "./lib/prisma"
- 
- 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./lib/prisma";
+import Credentials from "next-auth/providers/credentials";
+import { loginSchema } from "./lib/schemas/loginSchema";
+import { getUserByEmail } from "./app/actions/authActions";
+import { compare } from "bcryptjs";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  callbacks: {
+    async session({token, session}) {
+      console.log(token);
+      console.log(session);
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
-  session: {strategy: "jwt"},
-  providers: [],
-})
+  session: { strategy: "jwt" },
+  providers: [
+    Credentials({
+      name: "credentials",
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      authorize: async (creds) => {
+        const validated = loginSchema.safeParse(creds);
+
+        if (validated.success) {
+          const { email, password } = validated.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !(await compare(password, user.passwordHash)))
+            return null;
+
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+});
